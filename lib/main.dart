8895101b2e081ba6e6ea957fbed1943391810dda
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-06-18 17:53:09 +0530

    Adding and Viewing Receipts Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:kanjoosmaster/screens/home_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'constants.dart';
 import './screens/screen.dart';
 
 Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDownloader.initialize();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
@@ -23,7 +25,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'KanjoosMaster',
       theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+        fontFamily: 'Poppins',
         scaffoldBackgroundColor: kBackgroundColor,
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
--- lib/widgets/expense_component.dart
+++ lib/widgets/expense_component.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Expense extends StatefulWidget {
   final String expenseId;
@@ -25,6 +30,11 @@ class Expense extends StatefulWidget {
 }
 
 class _ExpenseState extends State<Expense> {
+  void _launchURL(String url) async {
+    // ignore: deprecated_member_use
+    await launch(url);
+  }
+
   final currentUser = FirebaseAuth.instance.currentUser;
   @override
   Widget build(BuildContext context) {
@@ -56,110 +66,248 @@ class _ExpenseState extends State<Expense> {
   }
 
   Future<void> expandedTransaction() async {
+    PlatformFile? pickedFile;
+
     await showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        backgroundColor: Colors.white,
-        title: Text(
-          "${widget.title}\n${widget.date}",
-          style: const TextStyle(
-              fontSize: 25, fontWeight: FontWeight.w800, color: Colors.black),
-        ),
-        content: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const Divider(
-                thickness: 1,
-                color: Colors.black,
-              ),
-              const SizedBox(height: 15),
-              Text(
-                widget.category,
-                style: const TextStyle(
-                    fontSize: 17.5, fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                widget.description,
-                style: const TextStyle(fontWeight: FontWeight.w400),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                "You ${widget.earning == true ? "earned" : "spent"} ${widget.amount} on this Transaction.",
-                style: const TextStyle(fontWeight: FontWeight.w400),
-              ),
-            ]),
-        actions: [
-          TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text(
-                "Cancel",
-                style: TextStyle(color: Colors.black),
-              )),
-          TextButton(
-              onPressed: () async {
-                showDialog(
-                  context: context,
-                  barrierDismissible: false,
-                  builder: (BuildContext context) {
-                    return const Dialog(
-                      child: Padding(
-                        padding: EdgeInsets.all(16.0),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            CircularProgressIndicator(),
-                            SizedBox(width: 16.0),
-                            Text('Deleting...'),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
+        context: context,
+        builder: (context) {
+          Future uploadFile() async {
+            final result = await FilePicker.platform.pickFiles();
+            if (result == null) {
+              return;
+            }
+            setState(() {
+              pickedFile = result.files.first;
+            });
+          }
+
+          Future addFile() async {
+            showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (BuildContext context) {
+                return const Dialog(
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        CircularProgressIndicator(),
+                        SizedBox(width: 16.0),
+                        Text('Adding Receipt...'),
+                      ],
+                    ),
+                  ),
                 );
-                try {
-                  CollectionReference expensesCollection =
-                      FirebaseFirestore.instance.collection('Expenses');
-                  DocumentReference documentRef =
-                      expensesCollection.doc(widget.expenseId);
-
-                  DocumentSnapshot<Map<String, dynamic>> snapshot =
-                      await documentRef.get()
-                          as DocumentSnapshot<Map<String, dynamic>>;
-
-                  if (snapshot.exists) {
-                    Map<String, dynamic> data = snapshot.data()!;
-
-                    List<dynamic> users = List.from(data['Users']);
-
-                    users.remove(currentUser!.email);
-
-                    await documentRef.update({'Users': users});
-
-                    if (users.isEmpty) {
-                      await documentRef.delete();
-                    }
-                  }
-
-                  // ignore: use_build_context_synchronously
-                  Navigator.pop(context);
-                  // ignore: use_build_context_synchronously
-                  Navigator.pop(context);
-                } catch (error) {
-                  // ignore: avoid_print
-                  print('Error deleting document: $error');
-                  Navigator.pop(context);
-                }
               },
-              child: const Text(
-                "Delete Transaction",
-                style: TextStyle(color: Colors.black),
-              )),
-        ],
-      ),
-    );
+            );
+            final path = "receipts/${widget.expenseId}/${pickedFile!.name}";
+            final file = File(pickedFile!.path!);
+            final ref = FirebaseStorage.instance.ref().child(path);
+            UploadTask? uploadTask = ref.putFile(file);
+            await uploadTask.whenComplete(() => {});
+            setState(() {
+              uploadTask = null;
+            });
+            // ignore: use_build_context_synchronously
+            Navigator.pop(context);
+          }
+
+          Future<List<String>> getReceiptUrls() async {
+            List<String> urls = [];
+
+            var result = await FirebaseStorage.instance
+                .ref('receipts/${widget.expenseId}')
+                .listAll();
+
+            for (var ref in result.items) {
+              String downloadUrl = await ref.getDownloadURL();
+              urls.add(downloadUrl);
+            }
+
+            return urls;
+          }
+
+          return StatefulBuilder(builder: ((context, setState) {
+            return AlertDialog(
+              backgroundColor: Colors.white,
+              title: Text(
+                "${widget.title}\n${widget.date}",
+                style: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.black),
+              ),
+              content: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Divider(
+                      thickness: 1,
+                      color: Colors.black,
+                    ),
+                    const SizedBox(height: 15),
+                    Text(
+                      widget.category,
+                      style: const TextStyle(
+                          fontSize: 17.5,
+                          fontWeight: FontWeight.w600,
+                          decoration: TextDecoration.underline),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      widget.description,
+                      style: const TextStyle(fontWeight: FontWeight.w400),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      "You ${widget.earning == true ? "earned" : "spent"} ${widget.amount} on this Transaction.",
+                      style: const TextStyle(fontWeight: FontWeight.w400),
+                    ),
+                    const SizedBox(height: 10),
+                    FutureBuilder<List<String>>(
+                      future: getReceiptUrls(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<List<String>> snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const CircularProgressIndicator();
+                        } else if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
+                        } else {
+                          List<String>? urls = snapshot.data;
+
+                          List<Widget> textWidgets = [];
+                          for (String url in urls!) {
+                            textWidgets.add(GestureDetector(
+                                onTap: () => _launchURL(url),
+                                child: const Text(
+                                  "Open Receipt",
+                                )));
+                            textWidgets.add(const SizedBox(height: 10));
+                          }
+
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Receipts: ',
+                                style: TextStyle(
+                                    fontSize: 17.5,
+                                    fontWeight: FontWeight.w600,
+                                    decoration: TextDecoration.underline),
+                              ),
+                              const SizedBox(height: 10),
+                              Column(children: textWidgets),
+                            ],
+                          );
+                        }
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    if (pickedFile != null)
+                      Column(
+                        children: [
+                          Text(
+                            pickedFile!.name,
+                          ),
+                          const SizedBox(height: 5),
+                          GestureDetector(
+                            onTap: () async {
+                              await addFile();
+                              setState(() {
+                                pickedFile = null;
+                              });
+                            },
+                            child: const Text(
+                              "Confirm Addition",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  color: Colors.blue),
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                        ],
+                      ),
+                    ElevatedButton(
+                        onPressed: () async {
+                          await uploadFile();
+                          setState(() {});
+                        },
+                        child: const Text("Add a Receipt"))
+                  ]),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      "Close",
+                      style: TextStyle(color: Colors.black),
+                    )),
+                TextButton(
+                    onPressed: () async {
+                      showDialog(
+                        context: context,
+                        barrierDismissible: false,
+                        builder: (BuildContext context) {
+                          return const Dialog(
+                            child: Padding(
+                              padding: EdgeInsets.all(16.0),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  CircularProgressIndicator(),
+                                  SizedBox(width: 16.0),
+                                  Text('Deleting...'),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                      try {
+                        CollectionReference expensesCollection =
+                            FirebaseFirestore.instance.collection('Expenses');
+                        DocumentReference documentRef =
+                            expensesCollection.doc(widget.expenseId);
+
+                        DocumentSnapshot<Map<String, dynamic>> snapshot =
+                            await documentRef.get()
+                                as DocumentSnapshot<Map<String, dynamic>>;
+
+                        if (snapshot.exists) {
+                          Map<String, dynamic> data = snapshot.data()!;
+
+                          List<dynamic> users = List.from(data['Users']);
+
+                          users.remove(currentUser!.email);
+
+                          await documentRef.update({'Users': users});
+
+                          if (users.isEmpty) {
+                            await documentRef.delete();
+                          }
+                        }
+
+                        // ignore: use_build_context_synchronously
+                        Navigator.pop(context);
+                        // ignore: use_build_context_synchronously
+                        Navigator.pop(context);
+                      } catch (error) {
+                        // ignore: avoid_print
+                        print('Error deleting document: $error');
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: const Text(
+                      "Delete Transaction",
+                      style: TextStyle(color: Colors.black),
+                    )),
+              ],
+            );
+          }));
+        });
   }
 }
